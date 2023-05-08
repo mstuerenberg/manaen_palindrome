91853956d6c75c24beb71dc077cc3ec677ae4642
manaen <manaeneth@gmail.com>
2023-05-08 13:12:23 +0200

    Factor into module, add integer palindrome detection

--- lib/manaen_palindrome.rb
+++ lib/manaen_palindrome.rb
@@ -2,23 +2,24 @@
 
 require_relative "manaen_palindrome/version"
 
-class String
-
-  # Returns true if palindrome, false otherwise.
+module ManaenPalindrome
+   # Returns true if palindrome, false otherwise.
   def palindrome?
     processed_content == processed_content.reverse
-  end
-
-  # Returns the letters in a string.
-  def letters
-    chars.select { |c| c.match(/[a-z]/i) }.join
   end
 
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      self.scan(/[a-z]/i).join.downcase
+      self.to_s.scan(/[a-z\d]/i).join.downcase
     end
+end
 
+class String
+  include ManaenPalindrome
+end
+
+class Integer
+  include ManaenPalindrome
 end
--- test/test_manaen_palindrome.rb
+++ test/test_manaen_palindrome.rb
@@ -23,4 +23,12 @@ class TestManaenPalindrome < Minitest::Test
     assert "Madam, I'm Adam.".palindrome?
   end
 
+  def test_integer_non_palindrome
+    refute 12345.palindrome?
+  end
+
+  def test_integer_palindrome
+    assert 12321.palindrome?
+  end
+
 end
